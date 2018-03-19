77a2d737aefbcf9c2c775ca5b083759f24678baf
Victoria Betts <toribetts@gmail.com>
2018-03-19 12:19:00 -0400

    Add ui components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+//Material UI imports
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import DatePicker from 'material-ui/DatePicker';
+import Slider from 'material-ui/Slider';
+import FlatButton from 'material-ui/FlatButton';
+//Planet data
 import PLANETS from './planets.json';
 
-let canvas_styles = {width: 1000, height: 1000}
+let canvas_styles = {width: 1000, height: 1000, margin: "auto", display:"block"}
 const ORBIT_RAD_FACTOR = 50
 const CENTER_X = canvas_styles.width/2
 const CENTER_Y = canvas_styles.height/2
-
-class Slider extends Component {
-	
-}
 
 class Canvas extends Component {
 	componentDidMount() {
 		const canvas = this.refs.canvas
 		const ctx = canvas.getContext("2d")
+		ctx.fillStyle = "black";
+		ctx.rect(0, 0, canvas_styles.width, canvas_styles.height)
+		ctx.fill()
 		this.draw(ctx)
 	}
 	draw (ctx){
@@ -51,11 +57,95 @@ class Canvas extends Component {
 	}
 }
 class App extends Component {
+	//render slider, btn, timerange with appropriate prop
+	constructor (props, context) {
+		    super(props, context)
+		    this.state = {
+				sliderValue: 0,
+				startDate: new Date("1970-01-01"),
+				endDate: new Date(),
+				isPlaying: false,
+				btnText: "play"
+			}
+		this.handleClick = this.handleClick.bind(this);
+	}
+	//Set slider value when user changes it
+	handleSliderChange = (event, value) => {
+		    this.setState({
+				sliderValue: value
+			})
+	};
+	//Start date picker
+	onStartChange = (event, date) => {
+		this.setState({ 
+			startDate: date
+		})
+	};
+	//End date picker
+	onEndChange = (event, date) => {
+		this.setState({ 
+			endDate: date
+		})
+	};
+	//Play button click
+	handleClick(){
+		if (this.state.isPlaying === true){
+			this.setState({
+				isPlaying : false,
+				btnText : "play"
+			})
+		} else {
+			this.setState({
+				isPlaying : true,
+				btnText : "pause"
+			})
+		}
+	}
+
 	render() {
+		const { sliderValue } = this.state
+
+		const btnContent = this.state.btnText
+
 		return (
-		<div className="main">
-			<Canvas />
-		</div>
+			<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)} >
+			<div className="main">
+				<div className="controlsContainer">
+					<div className="slider">
+						<Slider 
+							min={0}
+							max={100}
+							step={1}
+							value={sliderValue}
+							onChange={this.handleSliderChange}
+						/>
+					</div>
+					<div className="calendars">
+						<div className="left">
+							<DatePicker
+								id="startdate"
+								value={this.state.startDate}
+								onChange = {this.onStartChange}
+							/>
+						</div>
+						<div className="right">
+							<DatePicker 
+								id="enddate"
+								value={this.state.endDate}
+								onChange = {this.onEndChange}
+							/>
+						</div>
+					</div>
+					<div className="btn">
+						<FlatButton label={btnContent} onClick={this.handleClick} />
+					</div>
+				</div>
+				<div id="canvasContainer">	
+					<Canvas />
+				</div>
+
+			</div>
+			</MuiThemeProvider>
 		);
 	}
 }
