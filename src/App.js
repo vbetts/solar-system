4e5962bde0eacb7cd4ff7739081ca7abe8431b14
Victoria Betts <toribetts@gmail.com>
2018-03-19 14:54:01 -0400

    Added ssome planet position calculations

--- src/App.js
+++ src/App.js
@@ -12,19 +12,36 @@ import PLANETS from './planets.json';
 
 let canvas_styles = {width: 1000, height: 1000, margin: "auto", display:"block"}
 const ORBIT_RAD_FACTOR = 50
+const UNIX_TO_YEAR = 60*60*24*365.24
 const CENTER_X = canvas_styles.width/2
 const CENTER_Y = canvas_styles.height/2
 
 class Canvas extends Component {
+	//props:
+	//startDate
+	//endDate
+	//sliderIncrement
+	//updatePlanets
 	componentDidMount() {
 		const canvas = this.refs.canvas
-		const ctx = canvas.getContext("2d")
+		this.draw(canvas)
+	}
+
+	componentWillReceiveProps(nextProps){
+		//each frame is one day
+		const canvas = this.refs.canvas
+		let ctx = canvas.getContext("2d")
+		if (nextProps.updatePlanets === true){
+			ctx.clearRect(0, 0, canvas_styles.width, canvas_styles.height)
+			this.draw(canvas);
+		}
+	}
+
+	draw (canvas){
+		let ctx = canvas.getContext("2d")
 		ctx.fillStyle = "black";
 		ctx.rect(0, 0, canvas_styles.width, canvas_styles.height)
 		ctx.fill()
-		this.draw(ctx)
-	}
-	draw (ctx){
 		for (var planet in PLANETS){
 			let orbit_radius = PLANETS[planet].orbit*ORBIT_RAD_FACTOR
 			ctx.strokeStyle = "#444444"
@@ -39,9 +56,22 @@ class Canvas extends Component {
 
 	draw_planet(ctx, planet){
 		let orbit_radius = planet.orbit*ORBIT_RAD_FACTOR
-		//the angle will change according to the time!	
-		let x = CENTER_X + orbit_radius * Math.cos(planet.a_rad)
-		let y = CENTER_Y + orbit_radius * Math.sin(planet.a_rad)
+
+		let unixStartTime = new Date(planet.date)
+		unixStartTime = unixStartTime.getTime()/1000
+		//There's not a quick and easy way to add days to a date in javascript
+		//You have to create the date then set it by adding the required number of days
+		let currentDate = new Date(this.props.startDate)
+		currentDate.setDate(currentDate.getDate()+this.props.sliderIncrement)
+		currentDate = currentDate.getTime()/1000
+		
+		let elapsedTime = (currentDate - unixStartTime)/UNIX_TO_YEAR
+		
+		let diff_rads = (elapsedTime/planet.o_period) * (2*Math.PI);
+		let rads = planet.a_rad + diff_rads
+		
+		let x = CENTER_X + orbit_radius * Math.cos(rads)
+		let y = CENTER_Y + orbit_radius * Math.sin(rads)
 		ctx.fillStyle = planet.color
 		ctx.beginPath()
 		ctx.arc(x, y, planet.radius, 0, 2*Math.PI)
@@ -57,12 +87,12 @@ class Canvas extends Component {
 	}
 }
 class App extends Component {
-	//render slider, btn, timerange with appropriate prop
 	constructor (props, context) {
+			//TODO: add max slider value= number of days between start date and end date
 		    super(props, context)
 		    this.state = {
 				sliderValue: 0,
-				startDate: new Date("1970-01-01"),
+				startDate: new Date("06/30/1970"),
 				endDate: new Date(),
 				isPlaying: false,
 				btnText: "play"
@@ -93,12 +123,15 @@ class App extends Component {
 			this.setState({
 				isPlaying : false,
 				btnText : "play"
-			})
+			});
 		} else {
 			this.setState({
 				isPlaying : true,
 				btnText : "pause"
-			})
+			});
+			//TODO: set timeout to 16ms (60FPS)
+			//in timeout, update slider each second
+			//check for max value of slider, if it is at max value set state.isPlaying to false
 		}
 	}
 
@@ -141,7 +174,12 @@ class App extends Component {
 					</div>
 				</div>
 				<div id="canvasContainer">	
-					<Canvas />
+					<Canvas 
+						sliderIncrement={this.state.sliderValue} 
+						startDate={this.state.startDate} 
+						endDate={this.state.endDate}
+						updatePlanets={this.state.isPlaying}
+					/>
 				</div>
 
 			</div>
