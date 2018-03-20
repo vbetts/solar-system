9751e3cfcb1bc793fe11e918bf97adc167e74082
Victoria Betts <toribetts@gmail.com>
2018-03-20 13:00:34 -0400

    Added dynamic planet updates when you move slider & date message

--- src/App.js
+++ src/App.js
@@ -7,92 +7,10 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import DatePicker from 'material-ui/DatePicker';
 import Slider from 'material-ui/Slider';
 import FlatButton from 'material-ui/FlatButton';
+import Snackbar from 'material-ui/Snackbar';
 //Planet data
-import PLANETS from './planets.json';
+import { Canvas } from './Canvas.js';
 
-let canvas_styles = {width: 1000, height: 1000, margin: "auto", display:"block"}
-const ORBIT_RAD_FACTOR = 50
-const UNIX_TO_YEAR = 60*60*24*365.24
-const CENTER_X = canvas_styles.width/2
-const CENTER_Y = canvas_styles.height/2
-
-class Canvas extends Component {
-	//props:
-	//startDate
-	//endDate
-	//sliderIncrement
-	//updatePlanets
-	componentDidMount() {
-		const canvas = this.refs.canvas
-		this.draw(canvas)
-	}
-
-	componentWillReceiveProps(nextProps){
-		//each frame is one day
-		const canvas = this.refs.canvas
-		let ctx = canvas.getContext("2d")
-		if (nextProps.updatePlanets === true){
-			ctx.clearRect(0, 0, canvas_styles.width, canvas_styles.height)
-			this.draw(canvas);
-		}
-	}
-
-	draw (canvas){
-		let ctx = canvas.getContext("2d")
-		ctx.fillStyle = "black";
-		ctx.rect(0, 0, canvas_styles.width, canvas_styles.height)
-		ctx.fill()
-		for (var planet in PLANETS){
-			let orbit_radius = PLANETS[planet].orbit*ORBIT_RAD_FACTOR
-			ctx.strokeStyle = "#444444"
-			ctx.beginPath()
-			//arc: x, y, radius, startangle, endangle
-			ctx.arc(CENTER_X, CENTER_Y, orbit_radius, 0, 2*Math.PI)
-			ctx.stroke()
-			//Planet
-			this.draw_planet(ctx, PLANETS[planet])
-		}
-	}
-
-	draw_planet(ctx, planet){
-		let orbit_radius = planet.orbit*ORBIT_RAD_FACTOR
-		
-		let rads = this.getPlanetPosition(planet)
-		
-		let x = CENTER_X + orbit_radius * Math.cos(rads)
-		let y = CENTER_Y + orbit_radius * Math.sin(rads)
-		ctx.fillStyle = planet.color
-		ctx.beginPath()
-		ctx.arc(x, y, planet.radius, 0, 2*Math.PI)
-		ctx.fill()
-		
-	}
-
-	getPlanetPosition(planet){
-		let unixStartTime = new Date(planet.date)
-		unixStartTime = unixStartTime.getTime()/1000
-		//There's not a quick and easy way to add days to a date in javascript
-		//You have to create the date then set it by adding the required number of days
-		let currentDate = new Date(this.props.startDate)
-		currentDate.setDate(currentDate.getDate()+this.props.sliderIncrement)
-		currentDate = currentDate.getTime()/1000
-		
-		let elapsedTime = (currentDate - unixStartTime)/UNIX_TO_YEAR
-		
-		let diff_rads = (elapsedTime/planet.o_period) * (2*Math.PI);
-		let rads = planet.a_rad + diff_rads
-
-		return rads
-	}
-
-	render() {
-		return(
-		<div>
-		<canvas ref="canvas" style={canvas_styles} width={canvas_styles.width} height={canvas_styles.height} />
-		</div>
-		)
-	}
-}
 class App extends Component {
 	constructor (props, context) {
 		    super(props, context)
@@ -103,21 +21,71 @@ class App extends Component {
 						Math.abs(initial_end.getTime() - initial_start.getTime())/(1000*3600*24)
 				);
 		    this.state = {
-				sliderValue: 0,
-				startDate: initial_start,
-				endDate: initial_end,
-				sliderMax: initial_sliderMax,
-				isPlaying: false,
-				btnText: "play"
+				sliderValue		:	0,
+				startDate		: 	initial_start,
+				endDate			: 	initial_end,
+				sliderMax		: 	initial_sliderMax,
+				isPlaying		:	false,
+				btnText			: 	"play",
+				openSnackbar	: 	false,
+				dateMsg			: 	""
 			}
 		this.handleClick = this.handleClick.bind(this);
 	}
 	//Set slider value when user changes it
 	handleSliderChange = (event, value) => {
-		    this.setState({
-				sliderValue: value
-			})
+		this.setState({
+			sliderValue: value
+		})
 	};
+
+
+	 handleRequestClose = () => {
+		 this.setState({
+				   openSnackbar: false,
+		});
+	};
+
+	showDate = (event) => {
+		const months = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December"
+		];
+
+		const days = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		];
+		let d = new Date(this.state.startDate);
+		d.setDate(d.getDate()+this.state.sliderValue);
+		
+		let msg= "Selected Date: "+ 
+		days[d.getDay()] + " " + 
+		months[d.getMonth()] + " " + 
+		d.getDate() + ", " + 
+		d.getFullYear();
+		this.setState({
+			openSnackbar: true,
+			dateMsg: msg
+		})
+		
+	}
+
 	//Start date picker
 	onStartChange = (event, date) => {
 		this.setState({ 
@@ -179,6 +147,7 @@ class App extends Component {
 		}
 	}
 
+
 	render() {
 		return (
 			<MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)} >
@@ -191,6 +160,7 @@ class App extends Component {
 							step={1}
 							value={this.state.sliderValue}
 							onChange={this.handleSliderChange}
+							onDragStop={this.showDate}
 						/>
 					</div>
 					<div className="calendars">
@@ -221,6 +191,12 @@ class App extends Component {
 						updatePlanets={this.state.isPlaying}
 					/>
 				</div>
+				<Snackbar
+				          open={this.state.openSnackbar}
+				          message={this.state.dateMsg}
+				          autoHideDuration={2000}
+				          onRequestClose={this.handleRequestClose}
+				        />
 
 			</div>
 			</MuiThemeProvider>
